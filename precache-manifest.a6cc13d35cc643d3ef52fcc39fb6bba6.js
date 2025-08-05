79f4f093b20acf9db09988e8d0a426bca729be4a
Thomas Roncin <thomas.roncin@sencrop.com>
2025-08-05 17:19:42 +0200

    Revert "murder-party"
    This reverts commit f27c3004767f09013fcd5a39ecf51542d8c92a89.

---
+++ precache-manifest.a6cc13d35cc643d3ef52fcc39fb6bba6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee0078ff02ea35e35420d4da8561cee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e22ea4b92efa3eb3d4c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f85bceaab6a74a22de37",
+    "url": "/static/js/2.aeeb7fe2.chunk.js"
+  },
+  {
+    "revision": "9776b30d41b546d9529276da9650fb84",
+    "url": "/static/js/2.aeeb7fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e22ea4b92efa3eb3d4c",
+    "url": "/static/js/main.bb84e7d7.chunk.js"
+  },
+  {
+    "revision": "13ad50102dc7ee27267e",
+    "url": "/static/js/runtime-main.585c513d.js"
+  }
+]);
---
+++ precache-manifest.e4d76e5a4609509df59fc27fe0749f79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35f3dac9cd75775fbff3640f3aa6039",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fd2b674f468da498859",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1bded345a09c2e18f0ad",
+    "url": "/static/js/2.8b89c2ba.chunk.js"
+  },
+  {
+    "revision": "9776b30d41b546d9529276da9650fb84",
+    "url": "/static/js/2.8b89c2ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd2b674f468da498859",
+    "url": "/static/js/main.9111d01d.chunk.js"
+  },
+  {
+    "revision": "13ad50102dc7ee27267e",
+    "url": "/static/js/runtime-main.585c513d.js"
+  }
+]);
